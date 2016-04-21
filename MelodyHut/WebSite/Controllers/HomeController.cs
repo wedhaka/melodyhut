e8fc2411f0c8f2bba3bb7bcf846d93cf4047a235
wedhaka <wedaka@gmail.com>
2016-04-21 19:47:09 +0530

    mvc format

---
+++ MelodyHut/WebSite/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult TestRequest() {
+            return Json("hi", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult TestPostRequest()
+        {
+            return Json("hi");
+        }
+
+        [HttpGet]
+        public JsonResult TestDataRequest(string name, int id)
+        {
+            return Json("hi", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MelodyHut/WebSite/Resources/Scripts/Home/Index.js
@@ -0,0 +1,22 @@
+﻿$.ajax({
+    url: '/Home/TestRequest/',
+    success: function (response) {
+        alert('hello');
+    }
+});
+
+$.ajax({
+    url: '/Home/TestPostRequest/',
+    method: 'post',
+    success: function (response) {
+        alert('hello Post');
+    }
+});
+
+$.ajax({
+    url: '/Home/TestDataRequest/',
+    data : {name: "Wedaka", id: 10},
+    success: function (response) {
+        alert(response);
+    }
+});
